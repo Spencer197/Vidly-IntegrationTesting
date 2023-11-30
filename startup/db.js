867a5958fa9c4450274ff99d20ff1426818b9352
Spencer197 <spncr@protonmail.com>
2023-11-30 20:07:38 +0900

    Add dynamic connection to DB

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = function() {
-  const db = config.get('db');
-  mongoose.connect(db)
-    .then(() => winston.info(`Connected to ${db}...`));
+  const db = config.get('db');//Dynamically read DB connection string as per running environment
+  mongoose.connect(db)//Dynamically connects to DB connection string
+    .then(() => winston.info(`Connected to ${db}...`));//`Template string` dynamically displays DB connection string 
 }
