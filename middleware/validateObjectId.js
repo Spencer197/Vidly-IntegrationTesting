924a0ae8580b5bd42783c3cb1f482508f374cf83
Spencer197 <spncr@protonmail.com>
2023-12-01 21:28:27 +0900

    Refactor genres.js to validateObjectId.js

--- middleware/validateObjectId.js
+++ middleware/validateObjectId.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = function(req, res, next) {
-  if (!mongoose.Types.ObjectId.isValid(req.params.id))
+  if (!mongoose.Types.ObjectId.isValid(req.params.id))//Lines 4-5 cut from genres.js - lines 46-47
     return res.status(404).send('Invalid ID.');
   
   next();
